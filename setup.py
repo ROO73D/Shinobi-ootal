3e437cc6081d6f8b00dcc7ecb539fc856b775ef6
ROO73D <73015074+ROO73D@users.noreply.github.com>
2023-11-18 16:18:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
+# from telethon.sync import TelegramClient
+# from telethon import TelegramClient, events
+# from telethon.sessions import StringSession
+# import re
+
+# api_id = 17484143
+# api_hash = 'b8c86dc2857d25f9b6c9af2e48739bfa'
+# string = '1BVtsOL8Bu1MYDQ0iWTrfOLyqhGp93Q8PUaykOJwYfd01H69cyYVl7ULlBEqplFH3sFHxR3cB3VCOJA8mmXOXN-_hjXtHlpzzVPE7W93Rex0Hkt5SsFbcdez8NIPorhw2y3VjIAVFFTrt3l1iqm4rccaphLuC8i60OT8hvXHpfQPJ3cVckVkRLJvtT_0qM8NaG_BQzsACMrj3hg-WFPpjqBf9lLE4KkqV8SsdQqsLKow0gH0Dd8KZdVOT746h_TNFzcCWq6nHg1tU8ZS5xi8B8WHzhSlJM_s1DLjOITKHTkw_fUjAQjo6Kvf7tkCM1QoYcc1gjTm2DXI05YLMVvzPKZmDLTZxXp8='
+
+# client = TelegramClient(StringSession(string), api_id, api_hash)
+
+# print('Auth finished ')
+# @client.on(events.NewMessage(chats=-1001803068113))
+# async def newMessageListener(event):
+#     msg = event.message
+#     if '/fuck' in msg.message:
+#       link = re.search(r'https:\/\/[a-zA-Z.\/1-90]+',msg.message).group(0)
+#       print(link)
+#       await client.send_message(-1001932519430,link)
+
+# with client:
+#   client.run_until_disconnected()
+
 from telethon.sync import TelegramClient
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
@@ -5,18 +28,17 @@ import re
 
 api_id = 17484143
 api_hash = 'b8c86dc2857d25f9b6c9af2e48739bfa'
-string = '1BVtsOL8Bu1MYDQ0iWTrfOLyqhGp93Q8PUaykOJwYfd01H69cyYVl7ULlBEqplFH3sFHxR3cB3VCOJA8mmXOXN-_hjXtHlpzzVPE7W93Rex0Hkt5SsFbcdez8NIPorhw2y3VjIAVFFTrt3l1iqm4rccaphLuC8i60OT8hvXHpfQPJ3cVckVkRLJvtT_0qM8NaG_BQzsACMrj3hg-WFPpjqBf9lLE4KkqV8SsdQqsLKow0gH0Dd8KZdVOT746h_TNFzcCWq6nHg1tU8ZS5xi8B8WHzhSlJM_s1DLjOITKHTkw_fUjAQjo6Kvf7tkCM1QoYcc1gjTm2DXI05YLMVvzPKZmDLTZxXp8='
-
+string = '1BVtsOLEBuyGiGM_RjIKROF55DZ8pcXlagi_qRb8Tlubga1crWxPdk3GEWC6P5zYV-r8Oa-SjWT63KIjALlASTfeO0F_kpTSvsUOZwSNg7VenbWnJtNWSz7yub4puYQiy-BaRBoYFYERo32Rvadj9nUadJxCiaVZHIca0qM-EtJilWkA4zIK6HmDyPp-rWzY_kdblaz4SzvX7Y-MAqayd0SSQelQJoORC4NINJAkTrPwNyjrSEjX-lrZuC6H1CogfC0HGdPEIh5Ibv3pQqI1BaXPn90qENajkzGziMvFowD79SMq-O8L-43L3gcz7c-BOySLB4mKDf3SOKnniGWfLfpnmx83y-8o='
 client = TelegramClient(StringSession(string), api_id, api_hash)
 
 print('Auth finished ')
-@client.on(events.NewMessage(chats=-1001803068113))
+@client.on(events.NewMessage(chats=-1001982143443))
 async def newMessageListener(event):
     msg = event.message
-    if '/fuck' in msg.message:
+    if 'https' in msg.message:
       link = re.search(r'https:\/\/[a-zA-Z.\/1-90]+',msg.message).group(0)
       print(link)
-      await client.send_message(-1001932519430,link)
+      await client.send_message(-1004046178315,link)
 
 with client:
   client.run_until_disconnected()
